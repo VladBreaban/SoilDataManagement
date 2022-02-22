d4b0824732a2c4a2a0dc148b2079f73deb33eab8
VladBreaban <vladbreaban@yahoo.com>
2022-02-22 17:03:05 +0000

    added and configuring nlog

--- SoilDataManagement/Controllers/WeatherForecastController.cs
+++ SoilDataManagement/Controllers/WeatherForecastController.cs
@@ -9,7 +9,7 @@ namespace SoilDataManagement.Controllers
         private static readonly string[] Summaries = new[]
         {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+         };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -21,6 +21,7 @@ namespace SoilDataManagement.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("Hello world");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
--- SoilDataManagement/Program.cs
+++ SoilDataManagement/Program.cs
@@ -1,7 +1,9 @@
+
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
-
+var fact = NLogBuilder.ConfigureNLog("nlog.config");
+var logger = fact.GetCurrentClassLogger();
+builder.Host.UseNLog();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
