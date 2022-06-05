63d41ae3f9b0efad8ce621c1ff5f57931f9833eb
VladBreaban <vladbreaban@yahoo.com>
2022-06-05 13:32:22 +0100

    search time interval

--- SoilDataManagement/Worker/Worker.cs
+++ SoilDataManagement/Worker/Worker.cs
@@ -2,10 +2,30 @@
 
 public class Worker : IWorker
 {
+    private readonly ILogger<Worker> _logger;
+    private readonly IDataManager _dataManager;
+    private readonly IDataCleaner _dataCleaner;
+    private readonly IElasticHelper _elasticHelper;
+    public Worker(ILogger<Worker> logger, IDataManager dataManager, IDataCleaner dataCleaner, IElasticHelper elasticHelper)
+    {
+        _logger = logger;
+        _dataManager = dataManager;
+        _dataCleaner = dataCleaner;
+        _elasticHelper = elasticHelper;
+    }
     public async Task DoWork(CancellationToken cancelToken)
     {
+        // to do--> make it configurable
+        TimeSpan start = new TimeSpan(20, 0, 0); //10 o'clock
+        TimeSpan end = new TimeSpan(20,10, 0); //12 o'clock
         while (!cancelToken.IsCancellationRequested)
         {
+            var now = DateTime.Now.TimeOfDay;
+            if ((now > start) && (now < end))
+            {
+                //match found --> get data from thinkspeak and send them to elastic server
+
+            }
 
         }
     }
